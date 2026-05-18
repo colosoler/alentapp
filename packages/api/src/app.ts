bd6e80577727a9a61ebd0b7ee421b3dcc5bf7a0c
MarianSalasT <marianosalas24@gmail.com>
2026-05-17 21:06:33 -0300

    feat(api): agregar endpoint DELETE /api/v1/lockers/:id en LockerController

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -45,6 +45,7 @@ import { GetLockersUseCase } from './application/GetLockersUseCase.js';
 import { RentLockerUseCase } from './application/RentLockerUseCase.js';
 import { ReleaseLockerUseCase } from './application/ReleaseLockerUseCase.js';
 import { UpdateLockerUseCase } from './application/UpdateLockerUseCase.js';
+import { DeleteLockerUseCase } from './application/DeleteLockerUseCase.js';
 
 export function buildApp() {
     const server = Fastify({
@@ -171,7 +172,8 @@ export function buildApp() {
     const rentLockersUseCase = new RentLockerUseCase(lockerRepo, memberRepo);
     const releaseLockersUseCase = new ReleaseLockerUseCase(lockerRepo);
     const updateLockerUseCase = new UpdateLockerUseCase(lockerRepo);
-    const lockerController = new LockerController(createLockerUseCase, getLockersUseCase, rentLockersUseCase, releaseLockersUseCase, updateLockerUseCase);
+    const deleteLockerUseCase = new DeleteLockerUseCase(lockerRepo);
+    const lockerController = new LockerController(createLockerUseCase, getLockersUseCase, rentLockersUseCase, releaseLockersUseCase, updateLockerUseCase, deleteLockerUseCase);
     const sportController = new SportController(createSportUseCase, getSportsUseCase, getSportByIdUseCase);
     const paymentController = new PaymentController(
         createPaymentUseCase,
@@ -272,6 +274,7 @@ export function buildApp() {
     server.patch('/api/v1/lockers/:id/rent', lockerController.rent.bind(lockerController));
     server.patch('/api/v1/lockers/:id/release', lockerController.release.bind(lockerController));
     server.patch('/api/v1/lockers/:id', lockerController.update.bind(lockerController));
+    server.delete('/api/v1/lockers/:id', lockerController.delete.bind(lockerController));
 
     server.get('/', async (req, rep) => {
         rep.status(200).send({ msg: 'asd' });
